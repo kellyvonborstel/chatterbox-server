d7aa116842f5b16d952813ed1c7987ef4b016b9e
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-05-04 00:33:40 -0700

    remove api keys

--- client/env/config.js
+++ client/env/config.js
@@ -7,8 +7,3 @@ if (!/(&|\?)username=/.test(window.location.search)) {
   newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
   window.location.search = newSearch;
 }
-
-$.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader("X-Parse-Application-Id", "wPfavNOxOKTzlXiMq9r226nSo5Vl5aMFfEcvAupE");
-  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "eCGqXWtDjoZofGzf785KvSODrUrXURbL6JCQqaHj");
-});
