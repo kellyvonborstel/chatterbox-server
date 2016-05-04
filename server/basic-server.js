ae781c42fa3dff111200ab92de4935090200ab83
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-05-04 16:50:02 -0700

    update routes path

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,27 +1,24 @@
 var http = require('http');
-var handleRequest = require('./request-handler');
-var urlParser = require('url');
+var messages = require('./request-handler');
 var utils = require('./utils');
+var url = require('url');
 
 var port = 3000;
 
 var ip = '127.0.0.1';
 
 var routes = {
-  '/classes/chatterbox': handleRequest
+  '/classes/messages': messages.requestHandler
 };
 
 var server = http.createServer(function(request, response) {
 
-  var urlParts = urlParser.parse(request.url);
-
-  var route = routes[urlParts.pathname];
-
+  var route = routes[url.parse(request.url).pathname];
   if (route) {
     route(request, response);
+  } else {
+    utils.sendResponse(response, '', 404);
   }
-
-  handleRequest(request, response);
 });
 
 console.log('Listening on http://' + ip + ':' + port);
