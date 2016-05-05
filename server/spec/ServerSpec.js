4913061d89546797aa0d333fc539bcbe584bb22e
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-05-04 18:57:49 -0700

    modify tests

--- server/spec/ServerSpec.js
+++ server/spec/ServerSpec.js
@@ -59,7 +59,7 @@ describe('Node Server Request Listener Function', function() {
   it('Should accept posts to /classes/room', function() {
     var stubMsg = {
       username: 'Jono',
-      message: 'Do my bidding!'
+      text: 'Do my bidding.'
     };
     var req = new stubs.request('/classes/room1', 'POST', stubMsg);
     var res = new stubs.response();
@@ -75,10 +75,10 @@ describe('Node Server Request Listener Function', function() {
     expect(res._ended).to.equal(true);
   });
 
-it('Should respond with messages that were previously posted', function() {
+  it('Should respond with messages that were previously posted', function() {
     var stubMsg = {
-      username: 'Jono',
-      message: 'Do my bidding!'
+      username: 'Amy',
+      text: 'No way!'
     };
     var req = new stubs.request('/classes/room1', 'POST', stubMsg);
     var res = new stubs.response();
@@ -97,7 +97,7 @@ it('Should respond with messages that were previously posted', function() {
     var messages = JSON.parse(res._data).results;
     expect(messages.length).to.be.above(0);
     expect(messages[0].username).to.equal('Jono');
-    expect(messages[0].message).to.equal('Do my bidding!');
+    expect(messages[0].text).to.equal('Do my bidding.');
     expect(res._ended).to.equal(true);
   });
 
